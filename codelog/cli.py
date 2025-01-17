e402af91e8e607f761c4c147acd0ebe3842e3bea
shahanxali <shahanali252002@gmail.com>
2025-01-17 19:11:10 +0530

    hashing

--- codelog/cli.py
+++ codelog/cli.py
@@ -1,10 +1,7 @@
-# def main():
-#     print("Hello World!")
-
-
-
 #argparse is for creating CLI commands, Program defining arguements and argparse parses it, here used for creating sub commands like git init or commit
+#sys to read from the file, all text from file
 import argparse
+import sys
 
 import os
 from . import data
@@ -15,19 +12,45 @@ def main ():
     args.func (args)
 
 
+# Parse Arguements (parse = extracting meaningfull components) basic
+# This function sets up the arguement parser for CLI (command line interface)
 def parse_args ():
     parser = argparse.ArgumentParser ()
 
     commands = parser.add_subparsers (dest='command')
     commands.required = True
 
+    # init setup
     init_parser = commands.add_parser ('init')
     init_parser.set_defaults (func=init)
+
+    # hashing setup
+    hash_object_parser = commands.add_parser ('hash-object')
+    hash_object_parser.set_defaults (func=hash_object)
+    hash_object_parser.add_argument ('file')
+
+    #cat file setup
+    cat_file_parser = commands.add_parser ('cat-file')
+    cat_file_parser.set_defaults (func=cat_file)
+    cat_file_parser.add_argument ('object')
 
     return parser.parse_args ()
 
 
+# init command for my git, the function to tell what to do when init is called
 def init (args):
 
     data.init ()
     print (f'Initialized empty CodeLog repository in {os.getcwd()}/{data.GIT_DIR}') #last one contains the info of directory
+
+
+# Compute and prints the hash of the file
+def hash_object (args):
+    with open (args.file, 'rb') as f:
+        print (data.hash_object (f.read ()))
+
+
+# Computes hash value to find real one
+def cat_file (args):
+    sys.stdout.flush ()
+    sys.stdout.buffer.write (data.get_object (args.object))
--- codelog/data.py
+++ codelog/data.py
@@ -1,8 +1,26 @@
+#For manipulation of directories and working realted to system
+
+import hashlib
 import os
 
 
 GIT_DIR = '.codelog'
 
-
+#created directory (.codelog (.git))
 def init ():
     os.makedirs (GIT_DIR)
+    os.makedirs (f'{GIT_DIR}/objects') # creation of directory to store hashes
+
+
+# Creation of OID (the SHA1 key)
+def hash_object (data):
+    oid = hashlib.sha1 (data).hexdigest ()
+    with open (f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write (data)
+    return oid
+
+
+# reads the OID, the cat-file, reverse of hashing
+def get_object (oid):
+    with open (f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+        return f.read ()
